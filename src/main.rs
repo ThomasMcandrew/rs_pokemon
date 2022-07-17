8c2fa99e059568c56a3a6b4958e67f5f83885bbc
ThomasMcAndrew <thomasmca86@gmail.com>
2022-07-17 08:53:47 -0500

    change to piston

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,100 @@
-use std::io;
-use std::{thread, thread::sleep, time::Duration};
-use std;
-use tui::{
-    backend::CrosstermBackend,
-    widgets::{ Widget, Block, Borders, canvas },
-    layout::{ Layout, Constraint, Direction, Rect },
-    Terminal,
-};
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
 
-struct Game {
-    window : Terminal<CrosstermBackend<io::Stdout>>,
-    is_running : bool,
-    x : u16,
-    y : u16,
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::*;
+use piston::window::WindowSettings;
+
+pub struct Game {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64,  // Rotation for the square.
+    playerx : f64,
+    playery : f64,
 }
-impl Game{
-    pub fn update(&mut self){
-        let input = self.get_user_input().expect("Failed to get input");
-        if input == 'c' { self.is_running = false } 
-        if input == 'w' { self.y = self.y - 1 }
-        if input == 's' { self.y = self.y + 1 }
-        if input == 'a' { self.x = self.x - 1 }
-        if input == 'd' { self.x = self.x + 1 }
+
+impl Game {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let square2 = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            let transform = c
+                .transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
+            let transform2 = c
+                .transform
+                .trans(self.playerx,self.playery);
+            rectangle(RED, square2, transform2, gl);
+            // Draw a box rotating around the middle of the screen.
+            rectangle(RED, square, transform, gl);
+        });
     }
-    pub fn render(&mut self) -> Result<(), io::Error> {
-        self.window.draw(|f| {
-            let size = f.size();
-            let block = Block::default()
-                .title("Block")
-                .borders(Borders::ALL);
-            f.render_widget(block, size);
-            let small_block = Block::default().borders(Borders::ALL);
-            f.render_widget(small_block, 
-                tui::layout::Rect::new(self.x,self.y,10,10));
-        })?;
-        return Ok(());
-    }
-    pub fn get_user_input(&mut self) -> Result<char, io::Error> {
-        if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Enter => {
-                        return Ok('\n');
-                    }
-                    KeyCode::Char(c) => {
-                        return Ok(c);
-                    }
-                    KeyCode::Backspace => {
-                        return Ok(' ');
-                    }
-                    KeyCode::Esc => {
-                        return Ok(' ');
-                    }
-                    _ => {
-                        return Ok(' ');
-                    }
+    fn update(&mut self, e: &Event) {
+        // Rotate 2 radians per second.
+        if let Some(args) = e.update_args() {
+            self.rotation += 2.0 * args.dt;
+        }
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            match key {
+                Key::W => self.playery = self.playery - 1.0,
+                Key::S => self.playery = self.playery + 1.0,
+                Key::A => self.playerx = self.playerx - 1.0,
+                Key::D => self.playerx = self.playerx + 1.0,
+                _ => (), 
             }
         }
-        Ok(' ')
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            match key {
+                Key::W => self.playery = self.playery - 1.0,
+                Key::S => self.playery = self.playery + 1.0,
+                Key::A => self.playerx = self.playerx - 1.0,
+                Key::D => self.playerx = self.playerx + 1.0,
+                _ => (), 
+            }
+        }
     }
 }
 
-fn main() -> Result<(), io::Error> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let terminal = Terminal::new(backend)?;
-  
+fn main() {
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create a Glutin window.
+    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
     let mut game = Game {
-       window : terminal,
-       is_running : true,
-       x : 0,
-       y : 0,
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+        playerx : 0.0,
+        playery : 0.0, 
     };
- 
-    while game.is_running {
-        game.update();
-        game.render()
-            .expect("Bad shit happened doodling");
-        // thread::sleep(Duration::new(2,0));
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            game.render(&args);
+        }    
+        game.update(&e);
     }
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        game.window.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    game.window.show_cursor()?;
-
-
-    Ok(())
-} 
-
-
-
+}
